c4bef6f23b5d33e45c67f9f2e687c8784553fdc8
박병휘 <bagbyeonghwi@parkbyeonghwi-MacBookAir.local>
2021-11-18 12:45:55 +0900

    [add] Point.h

---
+++ Point.h
@@ -0,0 +1,8 @@
+#ifndef _POINT_H_
+#define _POINT_H_
+
+struct Point {
+    int x, y;
+}
+
+#endif  // _POINT_H_
--- Util.h
+++ Util.h
@@ -1,18 +1,25 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+using std::string;
+
 class Util {
  public:
   Util* GetInstance();
-  void MakeBoard();
-  void Find(Player player);
-  void Select(Player player);
-  void Change(Player player, int x, int y);
-  bool IsFinished();
+  void Init();
+  bool SetName(string name1, string name2);
+  bool MakeBoard(int size);
+  void TogglePlayer();
+  void PrintBoard();
+  bool ShowValidXY(Player player);
+  bool InputXY();
+  void ToggleStone(int x, int y);
 
  private:
   Util();
   Util* intstance_;
+  int x_, y_, is_finished_;
+  Player* now_player_;
 };
 
 #endif  // _UTIL_H_
