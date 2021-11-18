a109a66c136e2da5ba9a88c4aaa939206cf1b972
ShinHyoung <jsin2475@naver.com>
2021-11-18 15:19:35 +0900

    [Modify] comments

--- Util.cpp
+++ Util.cpp
@@ -11,7 +11,7 @@ using std::string;
 
 void Init(){
 	// 선 플레이어 A로 가정.
-	
+
 
 	SetName();
 	MakeBoard();  
@@ -30,10 +30,21 @@ bool SetName(){
 }
 
 bool MakeBoard(){
-	// 보드의 사이즈 입력 (짝수 && num >=4)
+	// 보드의 사이즈 입력 (짝수 && num >=4 && num <= 20)
 	// cin을 통해 입력
-	// Borad instance_를 호출해서 배열 생성.
+	// board_ instance_를 호출해서 배열 생성.
 
+}
+
+bool InputXY(){
+		// 입력을 받는 공간. 
+		// cin을 통해 플레이어가 놓고 싶은 위치를 입력 받음.
+		// 예외 조건을 따져서 true, false를 출력
+		// 입력 받은 x, y값이
+		// 1. 보드 내부의 값
+		// 2. 보드의 값이 3
+		// 인지 확인을 해서 둘 다 만족하면 true, 아니면 false
+  
 }
 
 void TogglePlayer(){
@@ -44,21 +55,23 @@ void TogglePlayer(){
 
 void PrintBoard(){
 		// Board 상황을 출력 board_ 객체를 이용.
-		// 내부의 벡터를 가져와서 출력
+		// board_에서 프린트 하는 메소드를 사용.
 }
 
 bool ShowValidXY(){
 		// 현재 Player가 놓을 수 있는 위치를 계산.
 		// flag를 통해 현재 플레이어가 누구인지 확인
 		// 상, 하, 좌, 우, 상좌, 상우, 하좌, 하우 8방향으로
-		// 뒤집을 수 있는 위치 존재하는 지 vecotr<vector<int, int>>에
+		// 뒤집을 수 있는 위치 존재하는 지 좌표로 비교에
 		// 저장하고 나서 길이를 체크 해서 불가능 한지 판단.
-}
+		// 
+		// 놓을 수 있는 공간을 3으로 업데이트
 
-bool InputXY(){
-  
 }
 
 void ToggleStone(int x, int y){
+		// 상, 하, 좌, 우, 상좌, 상우, 하좌, 하우 8방향으로
+		// 뒤집을 수 있는 위치인지 판단
+		// 뒤집을 수 있는 경우 해당 방향에 존재하는 상대팀 돌을 뒤집기.
 
 }
