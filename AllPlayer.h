0cbfca845711faf473166af7d1ba9b1fac66fa58
박병휘 <bagbyeonghwi@parkbyeonghwi-MacBookAir.local>
2021-11-18 14:48:54 +0900

    [Init] update

--- AllPlayer.h
+++ AllPlayer.h
@@ -7,17 +7,7 @@ using std::string;
 
 class AllPlayer : public Info {
  public:
-  void SetScore();
-  int GetScore();
-  void SetName();
-  string GetName();
-  void SetColor();
-  string GetColor();
-
- private:
-  int score_;
-  string name_;
-  string color_;
+  void GetInfo() override();
 };
 
 #endif  // _ALLPLAYER_H_
