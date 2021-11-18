6b094b6cefd13ec30710d3811e9182dfb5b058f1
Nicholas <minibmw8205@gmail.com>
2021-11-18 16:00:53 +0900

    [add] Board

---
+++ Board.cpp
@@ -0,0 +1,48 @@
+#include "Board.h"
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::cout;
+
+Board::Board() {}
+Board* Board::instance_ = nullptr;
+Board* Board::GetInstance() {
+  if(instance_ == nullptr) {
+    instance_ = new Board();
+  }
+  return instance_;
+}
+
+vector< vector<int> > Board::GetBoard(){
+  return Board::board_;
+}
+
+void Board::SetBoard(int size){
+  vector< vector<int> > tmp_vector;
+  for (int i = 0; i < size; i++){
+    tmp_vector.push_back(vector<int>(size, 0));
+  }
+  board_ = tmp_vector;
+}
+
+void Board::ReturnBoard(){
+  vector< vector<int> > tmp_vector = Board::GetBoard();
+  for (int i = 0; i < tmp_vector.size() ; i++){
+    for(int j = 0; j < tmp_vector[i].size(); j++) {
+      if (tmp_vector[i][j] == 3) {
+        tmp_vector[i][j] = 0;
+      }
+    }
+  }
+}
+
+void Board::ShowBoard(){
+  vector< vector<int> > tmp_vector = Board::GetBoard();
+  for (int i = 0; i < tmp_vector.size() ; i++){
+    for(int j = 0; j < tmp_vector[i].size(); j++) {
+      cout << tmp_vector[i][j] << " ";
+    }
+    cout << "\n";
+  }
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 #include "Util.h"
+#include "Board.h"
 
 int main() {
     Util util;
-
+    Board* board_util = Board::GetInstance();
+    int size = 10;
+    board_util->SetBoard(size);
+    board_util->ShowBoard();
+    
     return 0;
 }
