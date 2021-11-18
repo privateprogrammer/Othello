59819cb4161a63ac3c3b54434f9be8b8a210d7b8
ShinHyoung <jsin2475@naver.com>
2021-11-18 15:02:26 +0900

    [Modify] comments

--- Util.cpp
+++ Util.cpp
@@ -9,13 +9,56 @@ using std::endl;
 using std::vector;
 using std::string;
 
-Util* Util::instance_ = NULL;
-Util::Util() {
+void Init(){
+	// 선 플레이어 A로 가정.
+	
+
+	SetName();
+	MakeBoard();  
+
+	while(1){
+
+		PrintBoard();
+	}
+	
 }
 
-Util* Util::GetInstance(){
-    if (instance_ == NULL){
-        instance_ = new Util();
-    }
-    return instance_;
+bool SetName(){
+ 	// 각 플레이어 이름 설정
+	// cin을 통해 입력을 받음 
+	// 이 플레이어들을 players에 저장.
+}
+
+bool MakeBoard(){
+	// 보드의 사이즈 입력 (짝수 && num >=4)
+	// cin을 통해 입력
+	// Borad instance_를 호출해서 배열 생성.
+
+}
+
+void TogglePlayer(){
+		// 플레이어 순서 변경을 위한 메소드
+		// flag를 통해 순서를 변경.
+		// 0, 1 idx를 통해서 가져오자.
+}
+
+void PrintBoard(){
+		// Board 상황을 출력 board_ 객체를 이용.
+		// 내부의 벡터를 가져와서 출력
+}
+
+bool ShowValidXY(){
+		// 현재 Player가 놓을 수 있는 위치를 계산.
+		// flag를 통해 현재 플레이어가 누구인지 확인
+		// 상, 하, 좌, 우, 상좌, 상우, 하좌, 하우 8방향으로
+		// 뒤집을 수 있는 위치 존재하는 지 vecotr<vector<int, int>>에
+		// 저장하고 나서 길이를 체크 해서 불가능 한지 판단.
+}
+
+bool InputXY(){
+  
+}
+
+void ToggleStone(int x, int y){
+
 }
--- Util.h
+++ Util.h
@@ -1,18 +1,29 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include <vector>
+#include "Point.h"
+#include "Player.h"
+#include "Board.h"
+
 using std::string;
 
 class Util {
  public:
+  Util* GetInstance();
   void Init();
   bool SetName();
   bool MakeBoard();
   void TogglePlayer();
   void PrintBoard();
-  bool ShowValidXY(Player player);
+  bool ShowValidXY();
   bool InputXY();
   void ToggleStone(int x, int y);
+
+ private:
+  int x_, y_, flag, is_finished_;
+  vector<Player> players_;
+  Board board_;
 };
 
 #endif  // _UTIL_H_
