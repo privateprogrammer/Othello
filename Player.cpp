cbf7b87a8c7e20c2c6ef01d8f0b088070147c3cb
ShinHyoung <jsin2475@naver.com>
2021-11-21 00:22:11 +0900

    [Modify] As header getter, setter is added

--- Player.cpp
+++ Player.cpp
@@ -10,7 +10,23 @@ using std::endl;
 using std::to_string;
 
 Player::Player(int score, string name, string color)
-: Info(score, name, color) {}
+: Info(name), score_(score), color_(color) {}
+
+void Player::SetColor(string color) { 
+  this->color_ = color; 
+}
+
+string Player::GetColor() { 
+  return this->color_; 
+}
+
+void Player::SetScore(int score) { 
+  this->score_ = score; 
+}
+
+int Player::GetScore() { 
+  return this->score_; 
+}
 
 void Player::GetInfo() {
   // 플레이어 정보 전달
