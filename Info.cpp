ace53215faf79407cf7f522da7c2a86861b8314e
ShinHyoung <jsin2475@naver.com>
2021-11-21 00:20:45 +0900

    [Modify] Inherit only name_

--- Info.cpp
+++ Info.cpp
@@ -5,13 +5,8 @@
 using std::string;
 
 // 생성자
-Info::Info(int score, string name, string color)
-: score_(score), name_(name), color_(color) {}
+Info::Info(string name) : name_(name){}
 
 // Getter Setter 지정
-void Info::SetScore(int score) { this->score_ = score; }
-int Info::GetScore() { return this->score_; }
 void Info::SetName(string name) { this->name_ = name; }
 string Info::GetName() { return this->name_; }
-void Info::SetColor(string color) { this->color_ = color; }
-string Info::GetColor() { return this->color_; }
