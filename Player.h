78d08339cada608d4be7ec8cfe1981c198a409c8
ShinHyoung <jsin2475@naver.com>
2021-11-21 00:21:29 +0900

    [Modify] fileds : score_, color_ add
    and getter, setter too

--- Player.h
+++ Player.h
@@ -10,7 +10,15 @@ using std::string;
 class Player : public Info {
  public:
   Player(int score, string name, string color);
+  void SetColor(string color);
+  string GetColor();
+  void SetScore(int score);
+  int GetScore();
   void GetInfo() override;
+
+ private:
+  int score_;
+  string color_;
 };
 
 #endif  // _PLAYER_H_
