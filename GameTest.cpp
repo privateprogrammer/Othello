c8b8e7bcc839a6a83f6dd86f3948d48691143ffe
ShinHyoung <jsin2475@naver.com>
2021-12-03 11:34:21 +0900

    [modify] check lint

--- GameTest.cpp
+++ GameTest.cpp
@@ -56,7 +56,7 @@ TEST(Test, AllPlayerSetScoreTEST) {
   EXPECT_EQ(tmp->GetScore(1), 300);
 }
 
-TEST(TEST, BoardTEST){
+TEST(TEST, BoardTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
 
@@ -72,34 +72,36 @@ TEST(TEST, BoardTEST){
   // 상태 2의 돌
   tmp_vector[base - 1][base] = 2;
   tmp_vector[base][base - 1] = 2;
-  
-  ASSERT_EQ(tmp_vector.size(), board_->GetBoard().size()) << "Vectors length are unequal";
+
+  ASSERT_EQ(tmp_vector.size(), board_->GetBoard().size())
+  << "Vectors length are unequal";
 
   for (int i = 0; i < board_->GetBoard().size(); ++i) {
-    EXPECT_EQ(tmp_vector[i], board_->GetBoard()[i]) << "Vectors are different" << i;
+    EXPECT_EQ(tmp_vector[i], board_->GetBoard()[i])
+    << "Vectors are different" << i;
   }
 }
 
-TEST(TEST, SetBoardTEST){
+TEST(TEST, SetBoardTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> SetBoard(1, 1, 1);
   EXPECT_EQ(board_->GetBoard()[1][1], 2);
 }
 
-TEST(TEST, ReturnBoardTEST){
+TEST(TEST, ReturnBoardTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> SetBoard(2, 1, 1);
   board_ -> ReturnBoard();
   EXPECT_EQ(board_->GetBoard()[1][1], 0);
 }
 
-TEST(TEST, IsBoardFullFalseTEST){
+TEST(TEST, IsBoardFullFalseTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   EXPECT_EQ(board_->IsBoardFull(), false);
 }
 
-TEST(TEST, IsBoardFullTrueTEST){
+TEST(TEST, IsBoardFullTrueTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   for (int i = 0; i < board_->GetBoard().size() ; i++)
@@ -110,48 +112,48 @@ TEST(TEST, IsBoardFullTrueTEST){
   EXPECT_EQ(board_->IsBoardFull(), true);
 }
 
-TEST(TEST, IsBoardOneColorTrueTEST){
+TEST(TEST, IsBoardOneColorTrueTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   for (int i = 0; i < board_->GetBoard().size() ; i++)
-    for (int j = 0; j < board_->GetBoard()[i].size(); j++){
+    for (int j = 0; j < board_->GetBoard()[i].size(); j++) {
       board_ ->SetBoard(1, i, j);
     }
   EXPECT_EQ(board_->IsBoardOneColor(), true);
 }
 
-TEST(TEST, IsBoardOneColorFalseTEST){
+TEST(TEST, IsBoardOneColorFalseTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   for (int i = 0; i < board_->GetBoard().size() ; i++)
-    for (int j = 0; j < board_->GetBoard()[i].size(); j++){
+    for (int j = 0; j < board_->GetBoard()[i].size(); j++) {
       board_ -> SetBoard(1, i, j);
     }
   board_ ->SetBoard(0, 0, 0);
   EXPECT_EQ(board_->IsBoardOneColor(), false);
 }
 
-TEST(TEST, BoardCheckMyScoreTEST){
+TEST(TEST, BoardCheckMyScoreTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   EXPECT_EQ(board_ -> CheckMyScore(0), 2);
 }
 
-TEST(TEST, UtilShowValidTEST1){
+TEST(TEST, UtilShowValidTEST1) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   Util util;
   EXPECT_EQ(util.ShowValidXY(0, 0), false);
 }
 
-TEST(TEST, UtilShowValidTEST2){
+TEST(TEST, UtilShowValidTEST2) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   Util util;
   EXPECT_EQ(util.ShowValidXY(0, 2), true);
 }
 
-TEST(TEST, UtilToggleStoneTEST2){
+TEST(TEST, UtilToggleStoneTEST2) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   Util util;
@@ -159,21 +161,21 @@ TEST(TEST, UtilToggleStoneTEST2){
   EXPECT_EQ(board_ -> GetBoard()[0][2], 1);
 }
 
-TEST(TEST, UtilUpdateScoreTEST){
+TEST(TEST, UtilUpdateScoreTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   Util util;
   util.ToggleStone(0, 2);
   util.UpdateScore();
-  
+
   EXPECT_EQ(util.allplayers()->GetScore(0), 4);
 }
 
-TEST(TEST, TogglePlayerTEST){
+TEST(TEST, TogglePlayerTEST) {
   Board* board_ = Board::GetInstance();
   board_ -> MakeBoard(4);
   Util util;
   util.TogglePlayer();
-  
+
   EXPECT_EQ(util.flag(), 1);
 }
